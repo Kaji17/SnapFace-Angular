5570cffcc025eafd99aa87ffaa445461b5162eb3
Ouattara Katina <ouattarapevirgile@gmail.com>
2022-10-18 14:56:17 +0000

    initialisation tableau de facesnap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,10 +6,83 @@ import { FaceSnaps } from './Model/FaceSnaps.model';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit{
-  mySnap!: FaceSnaps;
+export class AppComponent implements OnInit {
+  faceSnapTab!: FaceSnaps[];
 
   ngOnInit(): void {
-    this.mySnap= new FaceSnaps("Leandre", "Mon Pétit frère tjr frais", "31 Mars 2022", 4, "https://expertphotography.b-cdn.net/wp-content/uploads/2020/08/social-media-profile-photos-3.jpg")
+    this.faceSnapTab = [
+      {
+        title: "Leandre",
+        description: "Mon Pétit frère tjr frais",
+        createdDate: new Date(),
+        snaps: 0,
+        imageUrl: "https://expertphotography.b-cdn.net/wp-content/uploads/2020/08/social-media-profile-photos-3.jpg",
+        location: "Abidjan"
+      },
+
+      {
+        title: "Tamegnon",
+        description: "Benzema you're the GOAT",
+        createdDate: new Date(),
+        snaps: 0,
+        imageUrl: "https://th.bing.com/th?id=OIF.IueyCyItQiMVKuo1RO6%2fsQ&pid=ImgDet&rs=1",
+        location: "Paris"
+      },
+
+      {
+        title: "Leandre",
+        description: "Mon Pétit frère tjr frais",
+        createdDate: new Date(),
+        snaps: 0,
+        imageUrl: "https://expertphotography.b-cdn.net/wp-content/uploads/2020/08/social-media-profile-photos-3.jpg",
+        location: "Abidjan"
+      },
+
+      {
+        title: "Ninho Officiel",
+        description: "Certifié Diamond",
+        createdDate: new Date(),
+        snaps: 300000,
+        imageUrl: "https://th.bing.com/th/id/R.e0fb901172a93a947bdaa52eed55047c?rik=dlRQvmEGPoRsng&pid=ImgRaw&r=0"
+      },
+
+      {
+        title: "Leandre",
+        description: "Mon Pétit frère tjr frais",
+        createdDate: new Date(),
+        snaps: 0,
+        imageUrl: "https://expertphotography.b-cdn.net/wp-content/uploads/2020/08/social-media-profile-photos-3.jpg",
+        location: "Abidjan"
+      },
+
+      {
+        title: "Tamegnon",
+        description: "Benzema you're the GOAT",
+        createdDate: new Date(),
+        snaps: 0,
+        imageUrl: "https://th.bing.com/th?id=OIF.IueyCyItQiMVKuo1RO6%2fsQ&pid=ImgDet&rs=1",
+        location: "Paris"
+      },
+
+      {
+        title: "Leandre",
+        description: "Mon Pétit frère tjr frais",
+        createdDate: new Date(),
+        snaps: 0,
+        imageUrl: "https://expertphotography.b-cdn.net/wp-content/uploads/2020/08/social-media-profile-photos-3.jpg",
+        location: "Abidjan"
+      },
+      
+      {
+        title: "Ninho Officiel",
+        description: "Certifié Diamond",
+        createdDate: new Date(),
+        snaps: 300000,
+        imageUrl: "https://th.bing.com/th/id/R.e0fb901172a93a947bdaa52eed55047c?rik=dlRQvmEGPoRsng&pid=ImgRaw&r=0"
+      }
+
+    ]
   }
 }
+
+
