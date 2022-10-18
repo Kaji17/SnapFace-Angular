a30ad23e203783a4caaf64243e8f4c2a79ab4ce5
Ouattara Katina <ouattarapevirgile@gmail.com>
2022-10-18 01:56:00 +0000

    fonction d'affichage de date

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -19,3 +19,56 @@ export class FaceSnapComponent implements OnInit{
   }
 
 }
+// fonction permettant d'écrire la date dans un format JJ MM YY
+function dateCustom(date: Date): String {
+  var result: string = "not defined";
+  var day: number = new Date().getDate();
+  var monthNumb: number = new Date().getMonth();
+  var month!: String;
+  var years: number = new Date().getFullYear();
+  switch (monthNumb) {
+    case 0:
+      month = "January"
+      break;
+    case 1:
+      month = "February"
+      break;
+    case 2:
+      month = "March"
+      break;
+    case 3:
+      month = "April"
+      break;
+    case 4:
+      month = "May"
+      break;
+    case 5:
+      month = "June"
+      break;
+    case 6:
+      month = "July"
+      break;
+    case 7:
+      month = "August"
+      break;
+    case 8:
+      month = "September"
+      break;
+    case 9:
+      month = "October"
+      break;
+    case 10:
+      month = "November"
+      break;
+    case 11:
+      month = "December"
+      break;
+    default:
+      break;
+  }
+
+  result = day + " " + month + " "+ years;
+  return result;
+}
+
+  
