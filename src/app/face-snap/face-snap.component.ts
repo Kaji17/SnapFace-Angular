a8f098ea4b364b2765be33f423e93c35640ac126
Ouattara Katina <ouattarapevirgile@gmail.com>
2022-10-18 00:09:05 +0000

    modif

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -1,8 +1,21 @@
-import { Component} from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-face-snap',
   templateUrl: './face-snap.component.html',
   styleUrls: ['./face-snap.component.scss']
 })
-export class FaceSnapComponent{}
+export class FaceSnapComponent implements OnInit{
+  title!: String;
+  description!: String;
+  createdDate!: Date;
+  snaps!: Number;
+
+  ngOnInit(){
+    this.title = "Berenger";
+    this.description = "best friend";
+    this.createdDate = new Date;
+    this.snaps = 6;
+  }
+
+}
