f33c96cc307e0996b25c2f21e92e59fffd70b543
Ouattara Katina <ouattarapevirgile@gmail.com>
2022-10-18 03:05:15 +0000

    ajout constructeur FaceSnaps au model

---
+++ src/app/Model/FaceSnaps.model.ts
@@ -0,0 +1,8 @@
+export class FaceSnaps{
+    constructor(public title: string, 
+                public description: string, 
+                public createdDate: string, 
+                public snaps: number, 
+                public imageUrl: string){
+    }
+}
