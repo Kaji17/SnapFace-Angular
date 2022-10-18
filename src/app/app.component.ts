efdaf201033f21f960fe210a39cc3e5076edfbe8
Ouattara Katina <ouattarapevirgile@gmail.com>
2022-10-18 03:01:50 +0000

    instantatition objet Facesnap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FaceSnaps } from './Model/FaceSnaps.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  mySnap!: FaceSnaps;
+
+  ngOnInit(): void {
+    this.mySnap= new FaceSnaps("Leandre", "Mon Pétit frère tjr frais", "31 Mars 2022", 4, "https://expertphotography.b-cdn.net/wp-content/uploads/2020/08/social-media-profile-photos-3.jpg")
+  }
 }
